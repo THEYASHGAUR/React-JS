4b0e2c3c0e946d24c9eedd109a03c8280618edc0
Yash Gaur <yashgaur2525@gmail.com>
2022-12-09 21:07:22 -0500

    greetings using react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+const name = 'yash gaur';
+const date = new Date(2022,3,11,14);
+const time = date.getHours();
+let greeting = "";
+
+if(time >= 1 && time <12) {
+    greeting = "Good Morning";
+}
+else if(time >= 12 && time <18){
+  greeting = "good afternoon";
+}
+else{
+  greeting = "good night";
+}
+
+
+
+const box1 = {
+  backgroundColor:"lightblue",
+  display:"inline-block",
+  fontSize:"30px",
+  padding:"15px",
+  color:"white"
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.Fragment>
+    {/* <App /> */}
+    {/* <h1 style={{ height:"6px", width:"1000px", color:"blue",fontSize:"100px"}}>my name is {name}</h1> */}
+    <p>{`Current time: ${time}`}</p>
+    
+    <div className="box" style={box1}>Hello Sir, {greeting}</div>
+
+    
+  </React.Fragment>
 );
 
 // If you want to start measuring performance in your app, pass a function
